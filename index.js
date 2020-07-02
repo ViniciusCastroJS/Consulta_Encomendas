cbb4bc308806f33c1bf1a75bc6719e65c4357fd3
Vinicius S. de Castro <contatoviniciussc@gmail.com>
2020-07-02 18:33:35 -0300

    Ajustando Webpack

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
-import './layout/style.sass';
-import './index.html';
+
 
 const select = e => document.querySelector(e);
 
 function Buscar(){
 
-  let user_id = select("#consulta").value
+  let user_numero = select("#consulta").value
   
   let options = { method: 'GET', mode: 'no-cors', cache: 'default'};
   let url = "http://localhost:3000/encomendas"
-  let encomendaAPI = [];
-
-  fetch(url)
-  .then( res => res.json())
-  .then(data => {
-    data.forEach( elem => encomendaAPI.push(elem))
-  })
+  let obj = {}
   
-  let Index = encomendaAPI.findIndex(elem => elem.id === user_id);
+  let encomendaAPI = async function (){ 
+    let encomenda = await fetch(url);
+    let dados = await encomenda.json();
+    const [ ...encomendas ] = dados;
+    obj.encomendas = encomendas;
 
-  console.log(encomendaAPI)
+    let Index = obj.encomendas.findIndex(elem => elem.numero == user_numero);
+    
+  if(Index !== -1){
+    const {cliente, valor, data, entregue} = obj.encomendas[Index];
 
+    let dia = data.slice(8,10)
+    let mes = data.slice(5,7)
+    let ano = data.slice(0,4)
+    let valorReal = valor.toFixed(2).toString().replace('.', ',')
 
-  if(Index){
-    const {cliente, valor, data, entregue} = encomendaAPI[Index]; 
-
+    select(".valor").innerHTML = `R$${valorReal}`
     select(".cliente").innerHTML = `${ cliente.id } - ${cliente.nome}`
-    select(".valor").innerHTML = `R$ ${ valor }`
-    select(".cliente").innerHTML = `${ data } / ${data}`
-
+    select(".data").innerHTML = `${dia}/${mes}/${ano}`
+    
     entregue ? 
-    select(".status").innerHTML = "Entregue" : 
-    select(".status").innerHTML = "Entregar";
-
-    select(".sucessMessage").style.display = "flex";
-
+      select(".status").innerHTML = "Entregue" : select(".status").innerHTML = "Entregar";
+    
+      select(".cliente").style.display = 'flex';
+      select(".valor").style.display = 'flex';
+      select(".data").style.display = 'flex';
+      select(".status").style.display = 'flex';
+    
   }else{
-   
-    select("errorMessage").style.display = "flex";
+    select(".errorMessage").style.display = 'flex';
+    select(".errorMessage2").style.display = 'flex';
   }
+  }();
 
+
+  select(".cliente").style.display = 'none';
+  select(".valor").style.display = 'none';
+  select(".data").style.display = 'none';
+  select(".status").style.display = 'none';
+  select(".errorMessage").style.display = 'none';
+  select(".errorMessage2").style.display = 'none';
 }
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,60 @@
-const webpack = require("webpack");
+const webpack = require("webpack")
+const MiniCSSExtract = require('mini-css-extract-plugin');
+const UglifyJS = require('uglifyjs-webpack-plugin');
+const Minimize = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
    mode: 'production',
-   entry: './index.js',
+   entry: './index.html',
    output: {
-      filename: 'index.js',
+      filename: 'index.html',
       path: __dirname + '/build'
    },
+
+   optimization: {
+    minimizer: [
+       new UglifyJS({
+        cache: true,
+        parallel: true
+       }),
+       new Minimize({})
+     ]
+   },
+
+
+   plugins: [
+     new MiniCSSExtract({
+       filename: 'style.css'
+     }),
+   ],
    
    module: {
-     rules: [{
-       test: /\.css$/,
+     rules: [
+      {
+        test: /\.png$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+            },
+          },
+        ],
+      }, 
+
+      {
+        test: /\.html$/i,
+        use: [{loader: 'html-loader'}],
+      },
+
+      {
+       test: /\.scss$/i,
        use: [
-         'style-loader',
-         'css-loader'
-       ]
+        MiniCSSExtract.loader,
+        'css-loader',
+        'sass-loader',
+      ]
      }]
    },
 }
